81a02b189f5e0510d01a5bbc731c7820d8303e2d
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2024-11-01 18:12:13 -0300

    feat(security): add csrf strategy

--- internal/api/auth.go
+++ internal/api/auth.go
@@ -4,7 +4,15 @@ import (
 	"net/http"
 
 	jsonutils "github.com/FelipeBelloDultra/go-bid/internal/json-utils"
+	"github.com/gorilla/csrf"
 )
+
+func (api *API) HandleGetCSRFToken(w http.ResponseWriter, r *http.Request) {
+	token := csrf.Token(r)
+	jsonutils.EncodeJSON(w, r, http.StatusOK, map[string]string{
+		"csrf_token": token,
+	})
+}
 
 func (api *API) AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
--- internal/api/routes.go
+++ internal/api/routes.go
@@ -13,8 +13,16 @@ func (api *API) BindRoutes() {
 		api.Sessions.LoadAndSave,
 	)
 
+	// csrfMiddleware := csrf.Protect(
+	// 	[]byte(os.Getenv("GOBID_CSRF_KEY")),
+	// 	csrf.Secure(false), // Dev only
+	// )
+
+	// api.Router.Use(csrfMiddleware)
+
 	api.Router.Route("/api", func(r chi.Router) {
 		r.Route("/v1", func(r chi.Router) {
+			// r.Get("/csrf-token", api.HandleGetCSRFToken)
 			r.Route("/users", func(r chi.Router) {
 				r.Post("/sign-up", api.handleSignUpUser)
 				r.Post("/sign-in", api.handleSignInUser)
