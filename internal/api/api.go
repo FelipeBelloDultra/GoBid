906a08fe2544fff81ebabc3c1a4423f878634924
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2024-10-31 21:00:35 -0300

    feat(api): implement first route

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,9 +1,11 @@
 package api
 
 import (
+	"github.com/FelipeBelloDultra/go-bid/internal/services"
 	"github.com/go-chi/chi/v5"
 )
 
 type API struct {
-	Router *chi.Mux
+	Router      *chi.Mux
+	UserService services.UserService
 }
--- internal/api/user-handlers.go
+++ internal/api/user-handlers.go
@@ -1,9 +1,39 @@
 package api
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
+
+	jsonutils "github.com/FelipeBelloDultra/go-bid/internal/json-utils"
+	"github.com/FelipeBelloDultra/go-bid/internal/services"
+	"github.com/FelipeBelloDultra/go-bid/internal/use-case/user"
+)
 
 func (api *API) handleSignUpUser(w http.ResponseWriter, r *http.Request) {
-	panic("not implemented")
+	data, problems, err := jsonutils.DecodeValidJSON[user.CreateUserReq](r)
+	if err != nil {
+		_ = jsonutils.EncodeJSON(w, r, http.StatusUnprocessableEntity, problems)
+		return
+	}
+
+	id, err := api.UserService.CreateUser(
+		r.Context(),
+		data.UserName,
+		data.Email,
+		data.Password,
+		data.Bio,
+	)
+	if err != nil {
+		if errors.Is(err, services.ErrDuplicatedEmailOrPassword) {
+			_ = jsonutils.EncodeJSON(w, r, http.StatusUnprocessableEntity, map[string]any{
+				"error": "email or username already exists",
+			})
+		}
+	}
+
+	_ = jsonutils.EncodeJSON(w, r, http.StatusCreated, map[string]any{
+		"id": id,
+	})
 }
 
 func (api *API) handleSignInUser(w http.ResponseWriter, r *http.Request) {
