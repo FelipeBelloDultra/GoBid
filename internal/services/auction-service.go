6a0b9dcd4c518943636ba7e27f2bc6ef13b7f8e6
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2024-11-02 13:18:06 -0300

    feat(http): create read and write websocket events

--- internal/services/auction-service.go
+++ internal/services/auction-service.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"log/slog"
 	"sync"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
@@ -18,13 +19,14 @@ const (
 	FailedToPlaceBid
 	NewBidPlaced
 	AuctionFinished
+	InvalidJSON
 )
 
 type Message struct {
-	Message string
-	Amount  float64
-	Kind    MessageKind
-	UserID  uuid.UUID
+	Message string      `json:"message,omitempty"`
+	Amount  float64     `json:"amount,omitempty"`
+	Kind    MessageKind `json:"kind"`
+	UserID  uuid.UUID   `json:"user_id,omitempty"`
 }
 
 type AuctionLobby struct {
@@ -60,14 +62,14 @@ func (r *AuctionRoom) broadcastMessage(m Message) {
 		if err != nil {
 			if errors.Is(err, ErrBidIsTooLow) {
 				if client, ok := r.Clients[m.UserID]; ok {
-					client.Send <- Message{Kind: FailedToPlaceBid, Message: ErrBidIsTooLow.Error()}
+					client.Send <- Message{Kind: FailedToPlaceBid, Message: ErrBidIsTooLow.Error(), UserID: m.UserID}
 				}
 				return
 			}
 		}
 
 		if client, ok := r.Clients[m.UserID]; ok {
-			client.Send <- Message{Kind: SuccessfullyPlacedBid, Message: "your bid was successfully placed"}
+			client.Send <- Message{Kind: SuccessfullyPlacedBid, Message: "your bid was successfully placed", UserID: m.UserID}
 		}
 
 		for id, client := range r.Clients {
@@ -81,6 +83,13 @@ func (r *AuctionRoom) broadcastMessage(m Message) {
 			}
 			client.Send <- newBidMessage
 		}
+	case InvalidJSON:
+		client, ok := r.Clients[m.UserID]
+		if !ok {
+			slog.Info("Client not found in hashmap", "userId", m.UserID)
+			return
+		}
+		client.Send <- m
 	}
 }
 
@@ -139,5 +148,85 @@ func NewClient(room *AuctionRoom, conn *websocket.Conn, userId uuid.UUID) *Clien
 		Conn:   conn,
 		Send:   make(chan Message, 512),
 		UserID: userId,
+	}
+}
+
+const (
+	maxMessageSize = 512
+	readDeadline   = 60 * time.Second
+	writeWait      = 10 * time.Second
+	pingPeriod     = (readDeadline * 9) / 10
+)
+
+func (c *Client) ReadEventLoop() {
+	defer func() {
+		c.Room.Unregister <- c
+		c.Conn.Close()
+	}()
+
+	c.Conn.SetReadLimit(maxMessageSize)
+	c.Conn.SetReadDeadline(time.Now().Add(readDeadline))
+	c.Conn.SetPongHandler(func(string) error {
+		c.Conn.SetReadDeadline(time.Now().Add(readDeadline))
+		return nil
+	})
+
+	for {
+		var m Message
+		m.UserID = c.UserID
+		err := c.Conn.ReadJSON(&m)
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				slog.Error("Unexpected close error", "error", err)
+				return
+			}
+
+			c.Room.Broadcast <- Message{
+				Kind:    InvalidJSON,
+				Message: "thus message should be valid JSON",
+				UserID:  m.UserID,
+			}
+			continue
+		}
+
+		c.Room.Broadcast <- m
+	}
+}
+
+func (c *Client) WriteEventLoop() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.Conn.Close()
+	}()
+
+	for {
+		select {
+		case <-ticker.C:
+			c.Conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.Conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				slog.Error("Unexpected write error", "error", err)
+				return
+			}
+		case message, ok := <-c.Send:
+			if !ok {
+				c.Conn.WriteJSON(Message{
+					Kind:    websocket.CloseMessage,
+					Message: "closing websocket connection",
+				})
+				return
+			}
+
+			if message.Kind == AuctionFinished {
+				close(c.Send)
+				return
+			}
+
+			c.Conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.Conn.WriteJSON(message); err != nil {
+				c.Room.Unregister <- c
+				return
+			}
+		}
 	}
 }
